1c0eb132ac442115ba87229c4ca31cdc27e16849
SH3RIFFss <149625610+SH3RIFFss@users.noreply.github.com>
2024-08-07 09:21:05 -0400

    att

--- JVtestes001/src/App.java
+++ JVtestes001/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         int num = 0;
-
+        String lang = "java";
         // WHILE
 
         // while (num < 15) {
@@ -18,8 +18,26 @@ public class App {
 
         // FOR
 
-        for (num = 0; num < 5; num++) {
-            System.out.println("teste de numero " + num);
+        // for (num = 0; num < 5; num++) {
+        //     System.out.println("teste de numero " + num);
+        // }
+        
+        // SWITCH CASE
+
+        switch (lang) {
+            case ("c"):
+                System.out.println("Suporte apenas a programacao estruturado!");
+                break;
+            case ("c++"):
+                System.out.println("suporte a programacao estruturada e orientada a objetos!");
+                break;
+            case ("java"):
+                System.out.println("suporte apenas a programacao orientada a objetos!");
+                break;
+            default:
+                System.out.println("[ERRO]:LINGUACEM INVALIDA");
+                break;
         }
     }
+
 }
