884c7b33d6edc9412d006418a711980369b1ccfc
SH3RIFFss <149625610+SH3RIFFss@users.noreply.github.com>
2024-08-06 23:14:58 -0400

    Update App.java

--- JVtestes001/src/App.java
+++ JVtestes001/src/App.java
@@ -1,8 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int num=0;
-        while(num<10){
-            System.out.println("teste numero"+num);
+        int num = 0;
+
+        // WHILE
+
+        // while (num < 15) {
+        // System.out.println("teste "+num);
+        // num++;
+        // }
+
+        // DO WHILE
+
+        // do {
+        // System.out.println("teste de numero " + num);
+        // num++;
+        // } while (num < 10);
+
+        // FOR
+
+        for (num = 0; num < 5; num++) {
+            System.out.println("teste de numero " + num);
         }
     }
 }
