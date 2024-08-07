2aa552700128ce215b20631035a13b1050483efc
SH3RIFFss <149625610+SH3RIFFss@users.noreply.github.com>
2024-08-06 22:36:13 -0400

    erro

---
+++ JVtestes001/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num=0;
+        while(num<10){
+            System.out.println("teste numero"+num);
+        }
+    }
+}
