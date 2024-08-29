364084e43e5d3298bb7ffbe07eb6343e0144ac82
Felipe Barros <149625610+SH3RIFFss@users.noreply.github.com>
2024-08-29 15:33:38 -0400

    java connection with mysql

---
+++ javaBDconnectios/src/App.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Connection c1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/mercado","root","Cud3cachorro!");
+        Statement st = c1.createStatement();
+        st.executeUpdate("insert into cliente values(4,'andre','m', 'and@gmail.com')");
+        st.close();
+        c1.close();
+    }
+}
