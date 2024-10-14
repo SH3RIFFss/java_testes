ad1ecf81ee31582e5692f5d00ef707136d80b6ff
Felipe Barros <149625610+SH3RIFFss@users.noreply.github.com>
2024-10-14 11:33:26 -0400

    client add

--- javaBDconnectios/src/App.java
+++ javaBDconnectios/src/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Connection c1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/mercado","root","Cud3cachorro!");
         Statement st = c1.createStatement();
-        st.executeUpdate("insert into cliente values(4,'andre','m', 'and@gmail.com')");
+        st.executeUpdate("insert into cliente values(5,'steve','m', 'steve@gmail.com')");
         st.close();
         c1.close();
     }
