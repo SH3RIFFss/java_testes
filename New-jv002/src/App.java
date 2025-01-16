ee244acb24c4662bf495f0be39b8c55d9c44eece
Felipe Barros <149625610+SH3RIFFss@users.noreply.github.com>
2025-01-16 15:05:05 -0400

    swing interactions

--- New-jv002/src/App.java
+++ New-jv002/src/App.java
@@ -1,10 +1,10 @@
-import javax.swing.*;
-import java.awt.event.*;
+import javax.swing.*; //biblioteca de janela.
+import java.awt.event.*; //biblioteca de eventos.
 
 // import java.awt.event.ActionEvent;
 public class App {
     public static void main(String[] args) throws Exception {
-        JFrame janela = new JFrame();
+        JFrame janela = new JFrame(); //deifiniçao da janela com a biblioteca.
 
         // conteudo da janela
         JButton btn = new JButton("Login");
@@ -12,20 +12,20 @@ public class App {
         JLabel txtSenha = new JLabel("Senha");
         JTextArea usuario = new JTextArea();
         JPasswordField senha = new JPasswordField();
-        txtUsuario.setBounds(50, 100, 100, 30);
+        txtUsuario.setBounds(50, 100, 100, 30);  
         usuario.setBounds(50, 130, 100, 30);
         txtSenha.setBounds(50, 200, 100, 30);
         senha.setBounds(50, 230, 100, 30);
         btn.setBounds(50, 300, 100, 30);
-        btn.addActionListener(new ActionListener() {
+        btn.addActionListener(new ActionListener() {  //definiçao da açao do botao com a biblioteca.
             @Override
             public void actionPerformed(ActionEvent e) {
-                String usr=usuario.getText();
-                String snh=new String(senha.getPassword());
+                String usr=usuario.getText();  //para captuarar o conteudo da textArea.
+                String snh=new String(senha.getPassword()); //para capturar o conteudo do passwordField.
                 System.out.printf("Logado com ususario: %s\nsenha %s: ",usr,snh);
             }
         });
-        janela.setLayout(null);
+        janela.setLayout(null);  //para definir a ocupaçao dos itens com a biblioteca.
         janela.add(txtUsuario);
         janela.add(usuario);
         janela.add(senha);
@@ -33,8 +33,8 @@ public class App {
         janela.add(btn);
 
         // tamanho da janela
-        janela.setBounds(50, 50, 300, 500);
-        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setBounds(50, 50, 300, 500); //para definir otamanho de um item com a biblioteca.
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //para definir o encerramento do programa com o fechamento da janela com a biblioteca.
         janela.setVisible(true);
 
     }
