d815028945dbcc1fce2ab96e7b22084abe2d3322
Felipe Barros <149625610+SH3RIFFss@users.noreply.github.com>
2025-02-24 15:40:58 -0400

    randon MATH

--- New-jv005/src/App.java
+++ New-jv005/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Randomizaçao usando a classe base MATH
+        int ini = 1;
+        int fim = 10;
+        double randonNumber = (int) (ini + Math.random() * (fim - ini));
+        System.out.println(randonNumber);
     }
 }
