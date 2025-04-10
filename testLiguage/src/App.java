f93b6b07f188e212889dfd0cd7b70bfbf4d79f43
Felipe Barros <149625610+SH3RIFFss@users.noreply.github.com>
2025-04-10 15:58:39 -0400

    update

--- testLiguage/src/App.java
+++ testLiguage/src/App.java
@@ -42,7 +42,7 @@ public class App {
         System.out.println(num1);
 
         for (var i = 0; i < num2; i++) { // for para iteraçao de numeros
-            System.out.println("\n"+txt);
+            System.out.println("\n" + txt);
         }
         System.out.printf("%d,%d\n", num3, num2);
 
@@ -59,25 +59,58 @@ public class App {
         // verificador de idade para votaçao apartir de uma idade aleatoia.
         var vota = "nao tem a obrigacao de votar";
         var NaoVota = "tem a obrigacao de votar";
-        var ini = 16;
+        var ini = 1;
         var fin = 80;
         int numRandon = (int) (Math.random() * (fin - ini + 1)) + ini;
         String votar = (numRandon >= 16 && numRandon < 18) || (numRandon > 70) ? vota : NaoVota;
-        System.out.printf("\nvoce tem %d anos de idade, logo a voce %s\n", numRandon, votar);
+        // System.out.printf("\nvoce tem %d anos de idade, logo a voce %s\n", numRandon,
+        // votar);
 
         // testes de estruturas condicionais.
         // estruturas condicionais simples.
-        int random=(int) (Math.random()*(50-1+1))+1;
-        if(random>=25){
-            System.out.printf("\nvoce tem %s carrinhos da hotweels, voce pode ser um colecionador!\n",random);
+        int random = (int) (Math.random() * (50 - 1 + 1)) + 1;
+        if (random >= 25) {
+            System.out.printf("\nvoce tem %s carrinhos da hotweels, voce pode ser um colecionador!\n", random);
         }
         // estruturas condicionais compostas.
-        if(random>=35){
-            System.out.printf("\nprodutor, pois possui %d galinhas em sua propriedade\n",random);
-        }else{
-            System.out.printf("\nconsumidor, pois possui apenas %d galinhas em sua propriedade.\n",random);
+        if (random >= 35) {
+            System.out.printf("\nprodutor, pois possui %d galinhas em sua propriedade\n", random);
+        } else {
+            System.out.printf("\nconsumidor, pois possui apenas %d galinhas em sua propriedade.\n", random);
+        }
+        // estruturas condicionais encadeada.
+        if (numRandon < 16) {
+            System.out.printf("\nsua idade eh %d, logo nao pode votar\n", numRandon);
+        } else if ((numRandon >= 16 && numRandon <= 18) || (numRandon >= 70)) {
+            System.out.printf("\nsua idade eh %d, logo seu voto eh opcional\n", numRandon);
+        } else {
+            System.out.printf("\nsua idade eh %d, logo seu voto eh obrigatorio\n", numRandon);
         }
 
+        // estruturas condicionais de multipla escolha.
+        var estado = "solido";
+        switch (estado) {
+            case "solido":
+                System.out.printf("\no estado eh %s\n", estado);
+                break;
+            case "liquido":
+                System.out.printf("\no estado eh %s\n", estado);
+                break;
+            case "gasoso":
+                System.out.printf("\no estado eh %s\n", estado);
+                break;
+            default:
+                System.out.println("nao tem estado cadastrado");
+                break;
+        }
+        // estruturas de repetiçao
+        var i = 1;
+        //estruturas com teste logico no inico.
+        while (i <= 5) {
+            System.out.println("\nlaco de numero: " + i);
+            i++;
+        }
+        //estruras co teste logico no final.
     }
 
 }
