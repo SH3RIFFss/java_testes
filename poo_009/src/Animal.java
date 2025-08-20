ee9157d8031063e9c341292ef085867f17454964
Felipe Barros <149625610+SH3RIFFss@users.noreply.github.com>
2025-08-20 17:40:54 -0400

    introdução ao polimorfismo

---
+++ poo_009/src/Animal.java
@@ -0,0 +1,33 @@
+public abstract class Animal {
+    private String raca;
+    private double peso;
+    private int idade;
+
+    public abstract void emitirSom();
+    public abstract void alimentar();
+    public abstract void locomover();
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+}
