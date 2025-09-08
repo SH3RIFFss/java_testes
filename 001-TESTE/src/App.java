1f4fd9efd5cac04df829ae2b96c9fb0b07eeb631
Felipe Barros <149625610+SH3RIFFss@users.noreply.github.com>
2025-09-08 10:31:00 -0400

    removendo codigo inutil

--- 001-TESTE/src/App.java
+++ 001-TESTE/src/App.java
@@ -1,29 +1,5 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        Map<String, List<String>> myMap=new HashMap<>();
-        System.out.println("tamanho inicial da lista : "+myMap.size());
-        List<String> amigosFelipe=new ArrayList<>();
-        amigosFelipe.add("alice");
-        amigosFelipe.add("joao");
-        amigosFelipe.add("alacir");
-        myMap.put("felipe", amigosFelipe);
-        List<String> amigosJoao=new ArrayList<>();
-        amigosJoao.add("claudia");
-        amigosJoao.add("dirceu");
-        amigosJoao.add("afonso");
-        myMap.put("joao", amigosJoao);
-        System.out.println("===adicionando=arquvos===");
-        System.out.println("tamanho inicial da lista : "+myMap.size());
-        System.out.println("tamanho inicial da lista : "+amigosFelipe.size());
-        for (String amigos : myMap.keySet()) {
-            System.out.println("os amigos de primeiro grau de "+amigos+" sao : "+myMap.get(amigos));
-        }
         
         Grafo meuGrafo=new Grafo();
         meuGrafo.addVertice("felipe");
@@ -36,32 +12,17 @@ public class App {
         meuGrafo.addVertice("jenny");
 
         meuGrafo.addAresta("felipe", "bob");
-        meuGrafo.addAresta("felipe", "clair");
-        meuGrafo.addAresta("felipe", "alacir");
-        meuGrafo.addAresta("clair", "thom");
+        meuGrafo.addAresta("felipe", "thom");
+        meuGrafo.addAresta("felipe", "jenny");
+        meuGrafo.addAresta("thom", "clair");
         meuGrafo.addAresta("clair", "jenny");
-        meuGrafo.addAresta("alacir", "peggy");
+        meuGrafo.addAresta("peggy", "alacir");
         meuGrafo.addAresta("bob", "anuj");
         meuGrafo.addAresta("bob", "peggy");
 
         meuGrafo.bfs("felipe");
+        meuGrafo.bfs("thom");
+        meuGrafo.bfs("peggy");
         
     }
-    // public static void verificarAmigos(List<String> lista){
-    //     for (String vendedor : lista) {
-    //         if (vendedor(vendedor)) {
-    //             System.out.println(vendedor+", eh vendedor(a)");
-    //         }else{
-    //             System.out.println(vendedor+", nao eh vendedor(a)");
-    //             lista.remove(vendedor);
-    //         }
-    //     }
-    // }
-    // public static boolean vendedor(String nome){
-    //     if (nome.contains("r")) {
-    //         return true;
-    //     }else{
-    //         return false;
-    //     }
-    // }
 }
--- 001-TESTE/src/Grafo.java
+++ 001-TESTE/src/Grafo.java
@@ -8,40 +8,40 @@ import java.util.Set;
 import java.util.Queue;
 
 public class Grafo {
-    private Map<String, List<String>> listaAdjancia;
+    private Map<String, List<String>> listaAdjancia; //delaração da tabela hash principal
 
     //construtor
     public Grafo() {
-        this.listaAdjancia = new HashMap<>(); //cria uma nova tabela hash
+        this.listaAdjancia = new HashMap<>(); //instancia uma nova tabela hash
     }
 
-    @param //marcador de conexão 
+    @param //marcador de conexão para criar entidades(entidade[valor,valor])
     public void addVertice(String vertice) { 
         listaAdjancia.putIfAbsent(vertice, new ArrayList<>()); //cria uma nova tabela caso o valor nao esteja vinculado a nenhuma chave 
     }
 
-    @param //
+    @param //marcador de conexão para criar relacionamentos(chave[valor],valor[chave])
     public void addAresta(String origem, String destino) {
-        addVertice(origem);
-        addVertice(destino);
-        listaAdjancia.get(origem).add(destino);
-        listaAdjancia.get(destino).add(origem);
+        addVertice(origem); //adiciona o objeto final 
+        addVertice(destino); //adiciona a conexão de chave e valor
+        listaAdjancia.get(origem).add(destino); //relaciona a chave a um valor 
+        listaAdjancia.get(destino).add(origem); //relaciona um valor a uma chave
     }
 
-    @param //
+    @param //marcador de conexão para verificar os nós percorridos e diferenciar as caracteristica especifica desejada
     public void bfs(String inicio) {
-        Set<String> visitados = new HashSet<>(); 
-        Queue<String> fila = new LinkedList<>();
-        fila.add(inicio);
-        visitados.add(inicio);
+        Set<String> visitados = new HashSet<>(); //cria uma tabela hash para itens ja visitados
+        Queue<String> fila = new LinkedList<>(); //cria uma lista encadeada dos valores a serem verificados
+        fila.add(inicio); //adiciona a lista encadeada o chave de relacionamentos desejada
+        visitados.add(inicio); //adiciona a tabela hash o valor que ja foi verificado 
         System.out.println("==iniciando busca em largura no item : " + inicio);
-        while (!fila.isEmpty()) {
-            String verticeAtual = fila.poll();
-            System.out.println(verticeAtual);
-            for (String vizinho : listaAdjancia.get(verticeAtual)) {
-                if (vendedor(vizinho)) {
+        while (!fila.isEmpty()) { //emquanto lista encadeada estiver vazia
+            String verticeAtual = fila.poll(); //cria a variavel que atualiza e remove itens da lista encadeada
+            System.out.println(verticeAtual); 
+            for (String vizinho : listaAdjancia.get(verticeAtual)) { //para cada item da tabela hash principas compara se os valores atendem as exigências
+                if (vendedor(vizinho)) { //cria as exigências 
                     // visitados.add(vizinho);
-                    fila.add(vizinho);
+                    fila.add(vizinho); //adiciona as exigências a lista encadeada
                 }
             }
         }
@@ -49,11 +49,7 @@ public class Grafo {
 
     }
 
-    private boolean vendedor(String nome) {
-        if (nome.contains("r")) {
-            return true;
-        } else {
-            return false;
-        }
+    private boolean vendedor(String nome) { 
+        return nome.contains("r");
     }
 }
